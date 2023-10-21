f5d39c8681807bccf60483135a6ae448ccb71ae8
Drexregion <96681590+Drexregion@users.noreply.github.com>
2023-10-21 15:49:55 +0000

    Added a new question with a python file

---
+++ questions/Poker_game.md
@@ -0,0 +1,7 @@
+# Pick a card
+
+## Question
+
+Welcome to **divine**! Here is the problem statement:
+
+You are playing a poker game and the final round that could cost you millions of dollars is to pick a card from a list of cards which is arranged in ascending order that contains a number that is given to you in the shortest amount of time, which way would you take to pick that card and in the shortest amount of time possible so that you can cash out millions of dollars!!!!
---
+++ util/divine-quest.py
@@ -0,0 +1,18 @@
+def pick_a_card(list_cards, card_number):
+    mid = len(list_cards)//2
+    first_pos = 0
+    last_pos = len(list_cards)-1
+
+    while True:
+        if list_cards[mid] > card_number:
+            last_pos = mid
+            new_cards = list_cards[first_pos,last_pos+1]
+            if mid == card_number:
+                return mid
+          
+        else:
+            first_pos = mid
+            list_cards = list_cards[first_pos,last_pos+1]
+            if mid == card_number:
+                return mid
+           
